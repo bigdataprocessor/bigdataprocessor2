e981dec8c5aaadb795687cba1aecc2a58820c2a6
Ashis R <ashis.r91@gmail.com>
2018-12-21 11:19:27 +0100

    Removed redundant UI thread pool service.

--- src/main/java/de/embl/cba/bigDataTools2/dataStreamingGUI/LazyLoadingCommand.java
+++ src/main/java/de/embl/cba/bigDataTools2/dataStreamingGUI/LazyLoadingCommand.java
@@ -22,91 +22,82 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Plugin(type = Command.class, menuPath = "Plugins>BigDataTools>BigImporter (Beta)", initializer = "init")
-public class LazyLoadingCommand<T extends RealType<T> & NativeType< T > > implements Command
-{
-	@Parameter
-	public static UIService uiService;
+public class LazyLoadingCommand<T extends RealType<T> & NativeType<T>> implements Command {
+    @Parameter
+    public static UIService uiService;
 
-	@Parameter
-	public DatasetService datasetService;
+    @Parameter
+    public DatasetService datasetService;
 
-	@Parameter
-	public LogService logService;
+    @Parameter
+    public LogService logService;
 
-	@Parameter
-	public OpService opService;
+    @Parameter
+    public OpService opService;
 
-	@Parameter
-	public StatusService statusService;
+    @Parameter
+    public StatusService statusService;
 
-	@Parameter( label = "Image data directory", style = "directory" )
-	File directory;
+    @Parameter(label = "Image data directory", style = "directory")
+    File directory;
 
-	@Parameter ( label = "Subset files using regular expression",
-			choices = {
-			".*",
-			".*--C.*",
-			".*Left.*",
-			".*Right.*",
-			".*short.*",
-			".*long.*",
-			".*Target.*",
-			".*LSEA00.*",
-			".*LSEA01.*"} )
-	String filterPattern = ".*";
+    @Parameter(label = "Subset files using regular expression",
+            choices = {
+                    ".*",
+                    ".*--C.*",
+                    ".*Left.*",
+                    ".*Right.*",
+                    ".*short.*",
+                    ".*long.*",
+                    ".*Target.*",
+                    ".*LSEA00.*",
+                    ".*LSEA01.*"})
+    String filterPattern = ".*";
 
-	@Parameter ( label = "Image files scheme",
-			choices = {
-			FileInfoConstants.SINGLE_CHANNEL_TIMELAPSE,
-			FileInfoConstants.LEICA_SINGLE_TIFF,
-			FileInfoConstants.LOAD_CHANNELS_FROM_FOLDERS,
-			FileInfoConstants.EM_TIFF_SLICES,
-			FileInfoConstants.PATTERN_1,
-			FileInfoConstants.PATTERN_2,
-			FileInfoConstants.PATTERN_3,
-			FileInfoConstants.PATTERN_4,
-			FileInfoConstants.PATTERN_5,
-			FileInfoConstants.PATTERN_6 })
-	String namingScheme = FileInfoConstants.SINGLE_CHANNEL_TIMELAPSE;
+    @Parameter(label = "Image files scheme",
+            choices = {
+                    FileInfoConstants.SINGLE_CHANNEL_TIMELAPSE,
+                    FileInfoConstants.LEICA_SINGLE_TIFF,
+                    FileInfoConstants.LOAD_CHANNELS_FROM_FOLDERS,
+                    FileInfoConstants.EM_TIFF_SLICES,
+                    FileInfoConstants.PATTERN_1,
+                    FileInfoConstants.PATTERN_2,
+                    FileInfoConstants.PATTERN_3,
+                    FileInfoConstants.PATTERN_4,
+                    FileInfoConstants.PATTERN_5,
+                    FileInfoConstants.PATTERN_6})
+    String namingScheme = FileInfoConstants.SINGLE_CHANNEL_TIMELAPSE;
 
-	@Parameter ( label = "Hdf5 data set name",
-			choices = {
-			"None",
-			"Data",
-			"Data111",
-			ImarisUtils.RESOLUTION_LEVEL + "0/Data",
-			ImarisUtils.RESOLUTION_LEVEL + "1/Data",
-			ImarisUtils.RESOLUTION_LEVEL + "2/Data",
-			ImarisUtils.RESOLUTION_LEVEL + "3/Data",
-			"ITKImage/0/VoxelData",
-			"Data222",
-			"Data444"})
-	String hdf5DataSet = "None";
+    @Parameter(label = "Hdf5 data set name",
+            choices = {
+                    "None",
+                    "Data",
+                    "Data111",
+                    ImarisUtils.RESOLUTION_LEVEL + "0/Data",
+                    ImarisUtils.RESOLUTION_LEVEL + "1/Data",
+                    ImarisUtils.RESOLUTION_LEVEL + "2/Data",
+                    ImarisUtils.RESOLUTION_LEVEL + "3/Data",
+                    "ITKImage/0/VoxelData",
+                    "Data222",
+                    "Data444"})
+    String hdf5DataSet = "None";
 
-	@Parameter ( label = "Image viewer",
-			choices = {
-			ViewerUtils.BIG_DATA_VIEWER
-			//ViewerUtils.IJ1_VIEWER
-			})
-	String imageViewerChoice = ViewerUtils.BIG_DATA_VIEWER;
+    @Parameter(label = "Image viewer",
+            choices = {
+                    ViewerUtils.BIG_DATA_VIEWER
+                    //ViewerUtils.IJ1_VIEWER
+            })
+    String imageViewerChoice = ViewerUtils.BIG_DATA_VIEWER;
 
-	private static final DataStreamingTools dataStreamingTools = new DataStreamingTools();
-	private ExecutorService uiActionThreadPool;
+    private static final DataStreamingTools dataStreamingTools = new DataStreamingTools();
 
-	public void init() {
-		this.uiActionThreadPool = Executors.newFixedThreadPool(4); //TODO --ashis
-	}
-
-	public void run()
-	{
-		uiActionThreadPool.submit(() -> dataStreamingTools.openFromDirectory(
-				directory.toString(),
-				namingScheme,
-				filterPattern,
-				hdf5DataSet,
-				ViewerUtils.getImageViewer( imageViewerChoice )
-		));
-
-	}
+    public void run() {
+        dataStreamingTools.openFromDirectory(
+                directory.toString(),
+                namingScheme,
+                filterPattern,
+                hdf5DataSet,
+                ViewerUtils.getImageViewer(imageViewerChoice));
+    }
 
 }
