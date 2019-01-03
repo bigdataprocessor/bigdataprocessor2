104b904f7a9fb1454b9481bd0b8122b10c3a04b9
Ashis R <ashis.r91@gmail.com>
2019-01-03 23:13:13 +0100

    Code Cleanup.

--- src/main/java/de/embl/cba/bigDataTools2/boundingBox/ShowBoundingBoxDialog.java
+++ src/main/java/de/embl/cba/bigDataTools2/boundingBox/ShowBoundingBoxDialog.java
@@ -6,6 +6,7 @@ import de.embl.cba.bigDataTools2.fileInfoSource.FileInfoConstants;
 import net.imglib2.Interval;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.util.Intervals;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -27,42 +28,41 @@ public class ShowBoundingBoxDialog {
     }
 
 
-
     public void show(RandomAccessibleInterval rai, final String buttonName, boolean includeTimeAxis) {
         final int[] min, max;
         if (includeTimeAxis) {
             min = new int[]{(int) rai.min(FileInfoConstants.X_AXIS_POSITION), (int) rai.min(FileInfoConstants.Y_AXIS_POSITION),
-                    (int) rai.min(FileInfoConstants.Z_AXIS_POSITION),(int)rai.min(FileInfoConstants.T_AXIS_POSITION)};
+                    (int) rai.min(FileInfoConstants.Z_AXIS_POSITION), (int) rai.min(FileInfoConstants.T_AXIS_POSITION)};
             max = new int[]{(int) rai.max(FileInfoConstants.X_AXIS_POSITION), (int) rai.max(FileInfoConstants.Y_AXIS_POSITION),
-                    (int) rai.max(FileInfoConstants.Z_AXIS_POSITION),(int)rai.max(FileInfoConstants.T_AXIS_POSITION)};
-        }else {
+                    (int) rai.max(FileInfoConstants.Z_AXIS_POSITION), (int) rai.max(FileInfoConstants.T_AXIS_POSITION)};
+        } else {
             min = new int[]{(int) rai.min(FileInfoConstants.X_AXIS_POSITION), (int) rai.min(FileInfoConstants.Y_AXIS_POSITION),
                     (int) rai.min(FileInfoConstants.Z_AXIS_POSITION)};
             max = new int[]{(int) rai.max(FileInfoConstants.X_AXIS_POSITION), (int) rai.max(FileInfoConstants.Y_AXIS_POSITION),
                     (int) rai.max(FileInfoConstants.Z_AXIS_POSITION)};
         }
-        long[] size= new long[FileInfoConstants.MAX_ALLOWED_IMAGE_DIMS];
+        long[] size = new long[FileInfoConstants.MAX_ALLOWED_IMAGE_DIMS];
         rai.dimensions(size);
-        int[] center = new int[]{(int)size[FileInfoConstants.X_AXIS_POSITION]/2 + min[BB_X_POS],(int)size[FileInfoConstants.Y_AXIS_POSITION]/2 + min[BB_Y_POS],(int)size[FileInfoConstants.Z_AXIS_POSITION]/2 + min[BB_Z_POS]}; // Center of the image stack.
-        int[] BBsize = new int[]{(int)size[FileInfoConstants.X_AXIS_POSITION]/4,(int)size[FileInfoConstants.X_AXIS_POSITION]/4,(int)size[FileInfoConstants.Z_AXIS_POSITION]/4}; //New BB 1/4th the size of the image.
-        if(BBsize[BB_Z_POS]<1){ // Check if Z goes below 1
-            BBsize[BB_Z_POS]=1;
+        int[] center = new int[]{(int) size[FileInfoConstants.X_AXIS_POSITION] / 2 + min[BB_X_POS], (int) size[FileInfoConstants.Y_AXIS_POSITION] / 2 + min[BB_Y_POS], (int) size[FileInfoConstants.Z_AXIS_POSITION] / 2 + min[BB_Z_POS]}; // Center of the image stack.
+        int[] BBsize = new int[]{(int) size[FileInfoConstants.X_AXIS_POSITION] / 4, (int) size[FileInfoConstants.X_AXIS_POSITION] / 4, (int) size[FileInfoConstants.Z_AXIS_POSITION] / 4}; //New BB 1/4th the size of the image.
+        if (BBsize[BB_Z_POS] < 1) { // Check if Z goes below 1
+            BBsize[BB_Z_POS] = 1;
         }
-        int[] minBB = new int[]{center[BB_X_POS]-BBsize[BB_X_POS]/2,center[BB_Y_POS]-BBsize[BB_Y_POS]/2,center[BB_Z_POS]-BBsize[BB_Z_POS]/2}; //Positioning the new BB at the center of the image.
-        int[] maxBB = new int[]{center[BB_X_POS]+BBsize[BB_X_POS]/2,center[BB_Y_POS]+BBsize[BB_Y_POS]/2,center[BB_Z_POS]+BBsize[BB_Z_POS]/2}; //Positioning the new BB at the center of the image.
-        final Interval initialInterval,rangeInterval;
+        int[] minBB = new int[]{center[BB_X_POS] - BBsize[BB_X_POS] / 2, center[BB_Y_POS] - BBsize[BB_Y_POS] / 2, center[BB_Z_POS] - BBsize[BB_Z_POS] / 2}; //Positioning the new BB at the center of the image.
+        int[] maxBB = new int[]{center[BB_X_POS] + BBsize[BB_X_POS] / 2, center[BB_Y_POS] + BBsize[BB_Y_POS] / 2, center[BB_Z_POS] + BBsize[BB_Z_POS] / 2}; //Positioning the new BB at the center of the image.
+        final Interval initialInterval, rangeInterval;
         final AtomicBoolean lock = new AtomicBoolean(false);
         final int boxSetupId = 8888; // some non-existing setup id TODO: make it static and increment for multiple use--ashis
         final String[] axesToCrop;
         String cropDialogTitleMessage;
-        if (includeTimeAxis){
+        if (includeTimeAxis) {
             cropDialogTitleMessage = "Select 4D section to crop";
-            initialInterval = Intervals.createMinMax(minBB[BB_X_POS], minBB[BB_Y_POS], minBB[BB_Z_POS],min[BB_T_POS],
-                        maxBB[BB_X_POS], maxBB[BB_Y_POS], maxBB[BB_Z_POS],max[BB_T_POS]); // the initially selected bounding box
-            rangeInterval = Intervals.createMinMax(min[BB_X_POS], min[BB_Y_POS], min[BB_Z_POS],min[BB_T_POS],
-                    max[BB_X_POS], max[BB_Y_POS], max[BB_Z_POS],max[BB_T_POS]);// the range (bounding box of possible bounding boxes)
+            initialInterval = Intervals.createMinMax(minBB[BB_X_POS], minBB[BB_Y_POS], minBB[BB_Z_POS], min[BB_T_POS],
+                    maxBB[BB_X_POS], maxBB[BB_Y_POS], maxBB[BB_Z_POS], max[BB_T_POS]); // the initially selected bounding box
+            rangeInterval = Intervals.createMinMax(min[BB_X_POS], min[BB_Y_POS], min[BB_Z_POS], min[BB_T_POS],
+                    max[BB_X_POS], max[BB_Y_POS], max[BB_Z_POS], max[BB_T_POS]);// the range (bounding box of possible bounding boxes)
             axesToCrop = FileInfoConstants.BOUNDING_BOX_AXES_4D;
-        }else {
+        } else {
             cropDialogTitleMessage = "Select 3D section to crop";
             initialInterval = Intervals.createMinMax(minBB[BB_X_POS], minBB[BB_Y_POS], minBB[BB_Z_POS],
                     maxBB[BB_X_POS], maxBB[BB_Y_POS], maxBB[BB_Z_POS]); // the initially selected bounding box
@@ -73,10 +73,10 @@ public class ShowBoundingBoxDialog {
 
 
         final CustomBoundingBoxDialog boundingBoxDialog =
-                new CustomBoundingBoxDialog(((BdvHandleFrame) bdv).getBigDataViewer().getViewerFrame(),cropDialogTitleMessage,
+                new CustomBoundingBoxDialog(((BdvHandleFrame) bdv).getBigDataViewer().getViewerFrame(), cropDialogTitleMessage,
                         ((BdvHandleFrame) bdv).getBigDataViewer().getViewer(),
                         ((BdvHandleFrame) bdv).getBigDataViewer().getSetupAssignments(), boxSetupId,
-                        initialInterval, rangeInterval,axesToCrop) {
+                        initialInterval, rangeInterval, axesToCrop) {
 
                     @Override
                     public void createContent() {
@@ -88,7 +88,7 @@ public class ShowBoundingBoxDialog {
                             @Override
                             public void actionPerformed(final ActionEvent e) {
                                 setVisible(false);
-                                    //System.out.println( "bounding box:" + BoundingBoxTools.printInterval( boxRealRandomAccessible.getInterval() ) );
+                                //System.out.println( "bounding box:" + BoundingBoxTools.printInterval( boxRealRandomAccessible.getInterval() ) );
                                 for (int d = 0; d < min.length; ++d) {
                                     min[d] = (int) boxRealRandomAccessible.getInterval().realMin(d);
                                     max[d] = (int) boxRealRandomAccessible.getInterval().realMax(d);
@@ -111,10 +111,8 @@ public class ShowBoundingBoxDialog {
                     private static final long serialVersionUID = 1L;
 
                 };
-        boundingBoxDialog.setModal(true);
         boundingBoxDialog.setModalityType(Dialog.ModalityType.MODELESS);
         boundingBoxDialog.setVisible(true);
-        //boundingBoxDialog.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         do {
             try {
                 synchronized (lock) {
