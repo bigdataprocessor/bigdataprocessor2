a0190bc38f38bf3d6118ef5a1cf51a3aa856cdd1
Christian Tischer <christian.tischer@embl.de>
2019-01-27 16:28:54 +0100

    Add 5D ShearTransform, is it slow though

--- src/main/java/de/embl/cba/bigDataTools2/dataStreamingGUI/BigDataConverter.java
+++ src/main/java/de/embl/cba/bigDataTools2/dataStreamingGUI/BigDataConverter.java
@@ -7,20 +7,22 @@ import de.embl.cba.bigDataTools2.saving.SaveCentral;
 import de.embl.cba.bigDataTools2.saving.SavingSettings;
 import de.embl.cba.bigDataTools2.utils.Utils;
 import de.embl.cba.bigDataTools2.viewers.ImageViewer;
-import net.imglib2.FinalInterval;
-import net.imglib2.FinalRealInterval;
-import net.imglib2.RandomAccessibleInterval;
-import net.imglib2.RealRandomAccessible;
+import net.imglib2.*;
 import net.imglib2.cache.img.CachedCellImg;
 import net.imglib2.converter.Converters;
 import net.imglib2.converter.RealUnsignedByteConverter;
+import net.imglib2.interpolation.randomaccess.ClampingNLinearInterpolatorFactory;
+import net.imglib2.interpolation.randomaccess.NearestNeighborInterpolatorFactory;
 import net.imglib2.realtransform.AffineRandomAccessible;
+import net.imglib2.realtransform.AffineTransform;
 import net.imglib2.realtransform.AffineTransform3D;
 import net.imglib2.realtransform.RealViews;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.RealType;
 import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.util.Intervals;
 import net.imglib2.util.Util;
+import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
 import java.util.ArrayList;
@@ -102,6 +104,9 @@ public class BigDataConverter {
     public static <T extends RealType<T> & NativeType<T>>
     RandomAccessibleInterval shearImage(RandomAccessibleInterval rai, ShearingSettings shearingSettings)
     {
+        System.out.println("Shear Factor X " + shearingSettings.shearingFactorX);
+        System.out.println("Shear Factor Y " + shearingSettings.shearingFactorY);
+
         List<RandomAccessibleInterval<T>> timeTracks = new ArrayList<>();
         int nTimeFrames = (int) rai.dimension( FileInfoConstants.T );
         int nChannels = (int) rai.dimension( FileInfoConstants.C );
@@ -123,6 +128,48 @@ public class BigDataConverter {
         final RandomAccessibleInterval sheared = Views.stack( timeTracks );
 //        System.out.println("Time elapsed (ms) " + (System.currentTimeMillis() - startTime));
         return sheared;
+    }
+
+    public static <T extends RealType<T> & NativeType<T>>
+    RandomAccessibleInterval shearImage5D( RandomAccessibleInterval rai5D, ShearingSettings shearingSettings)
+    {
+        final AffineTransform affine5D = new AffineTransform( 5 );
+        affine5D.set(shearingSettings.shearingFactorX, 0, 2);
+        affine5D.set(shearingSettings.shearingFactorY, 1, 2);
+
+        RealRandomAccessible rra = Views.interpolate(
+                Views.extendZero( rai5D ),
+                new NearestNeighborInterpolatorFactory());
+
+        AffineRandomAccessible af = RealViews.affine( rra, affine5D );
+
+        final FinalInterval interval5D = getInterval5DAfterShearing( rai5D, shearingSettings );
+
+        RandomAccessibleInterval intervalView = Views.interval( af, interval5D );
+
+        return intervalView;
+    }
+
+    private static FinalInterval getInterval5DAfterShearing(
+            RandomAccessibleInterval rai5D,
+            ShearingSettings shearingSettings )
+    {
+        AffineTransform3D affine3DtoEstimateBoundsAfterTransformation = new AffineTransform3D();
+        affine3DtoEstimateBoundsAfterTransformation.set(shearingSettings.shearingFactorX, 0, 2);
+        affine3DtoEstimateBoundsAfterTransformation.set(shearingSettings.shearingFactorY, 1, 2);
+
+
+        final IntervalView intervalView3D = Views.hyperSlice( Views.hyperSlice( rai5D, FileInfoConstants.T, 0 ), FileInfoConstants.C, 0 );
+        FinalRealInterval transformedRealInterval = affine3DtoEstimateBoundsAfterTransformation.estimateBounds( intervalView3D );
+
+        final Interval interval = Intervals.largestContainedInterval( transformedRealInterval );
+        final long[] min = new long[ 5 ];
+        final long[] max = new long[ 5 ];
+        rai5D.min( min );
+        rai5D.max( max );
+        interval.min( min );
+        interval.max( max );
+        return new FinalInterval( min, max );
     }
 
 
--- src/test/java/TestShearTransform.java
+++ src/test/java/TestShearTransform.java
@@ -12,7 +12,8 @@ import net.imglib2.type.numeric.integer.UnsignedShortType;
 
 public class TestShearTransform {
 
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
         String imageDirectory = "src/test/resources/shear_transform_test";
         final FileInfoSource fileInfoSource = new FileInfoSource( imageDirectory, FileInfoConstants.SINGLE_CHANNEL_TIMELAPSE,
                 ".*", "", true);
@@ -34,6 +35,19 @@ public class TestShearTransform {
         imageViewer.show( sheared, imageViewer.getVoxelSize(), "sheared" );
         imageViewer.setDisplayRange( 0, 800, 0 );
 
+        /**
+         * Compute shearing using a 5D Affine Transform.
+         * This is much simpler, but also much  slower to compute
+         */
+    //        RandomAccessibleInterval sheared5D = getShearedImage5D( cachedCellImg, imageViewer );
+    //        ImageViewer imageViewer2 = new BdvImageViewer<UnsignedShortType>(
+    //                sheared5D,
+    //                "sheared5D",
+    //                new double[]{1.0, 1.0, 1.0});
+    //        imageViewer2.show();
+    //        imageViewer2.setDisplayRange( 0, 800, 0 );
+
+
     }
 
     private static RandomAccessibleInterval getShearedImage( CachedCellImg cachedCellImg, ImageViewer imageViewer )
@@ -42,5 +56,13 @@ public class TestShearTransform {
         ObliqueMenuDialog dialog = new ObliqueMenuDialog(imageViewer);
         dialog.getShearingSettings( shearingSettings ); // sets default values.
         return BigDataConverter.shearImage( cachedCellImg, shearingSettings );
+    }
+
+    private static RandomAccessibleInterval getShearedImage5D( CachedCellImg cachedCellImg, ImageViewer imageViewer )
+    {
+        ShearingSettings shearingSettings = new ShearingSettings();
+        ObliqueMenuDialog dialog = new ObliqueMenuDialog(imageViewer);
+        dialog.getShearingSettings( shearingSettings ); // sets default values.
+        return BigDataConverter.shearImage5D( cachedCellImg, shearingSettings );
     }
 }
