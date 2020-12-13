ab10c4ad6fda78a8160d123442705a27a1550162
Christian Tischer <christian.tischer@embl.de>
2020-12-13 18:33:32 +0100

    Add voxel dimensions to test

--- src/test/java/test/open/TestOpenMultiChannelZLIBCompressedTiffVolumes.java
+++ src/test/java/test/open/TestOpenMultiChannelZLIBCompressedTiffVolumes.java
@@ -1,7 +1,10 @@
 package test.open;
 
 import de.embl.cba.bdp2.BigDataProcessor2;
+import de.embl.cba.bdp2.BigDataProcessor2UI;
 import de.embl.cba.bdp2.image.Image;
+import de.embl.cba.bdp2.scijava.Services;
+import net.imagej.ImageJ;
 import org.junit.Test;
 
 import static de.embl.cba.bdp2.open.NamingSchemes.*;
@@ -10,6 +13,10 @@ public class TestOpenMultiChannelZLIBCompressedTiffVolumes
 {
     public static void main(String[] args)
     {
+        ImageJ imageJ = new ImageJ();
+        Services.setContext( imageJ.getContext() );
+        Services.setCommandService( imageJ.command() );
+        BigDataProcessor2UI.showUI();
         new TestOpenMultiChannelZLIBCompressedTiffVolumes().run();
     }
 
@@ -19,5 +26,9 @@ public class TestOpenMultiChannelZLIBCompressedTiffVolumes
         final String directory = "src/test/resources/test/tiff-nc2-nt6-zlib";
 
         final Image image = BigDataProcessor2.openTiffSeries( directory, MULTI_CHANNEL_VOLUMES + OME_TIF );
+
+        image.setVoxelDimensions( 1,1,1 );
+
+        BigDataProcessor2.showImage( image );
     }
 }
